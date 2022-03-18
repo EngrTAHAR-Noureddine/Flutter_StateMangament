88ba2d58ca709da480421994e95cc32ad630bb35
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-03-18 21:17:59 +0100

    add riverpod repo

---
+++ my_flutter_riverpods/android/app/src/main/kotlin/com/engrtaharnoureddine/my_flutter_riverpods/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.engrtaharnoureddine.my_flutter_riverpods
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- my_flutter_riverpods/lib/myapp/Home.dart
+++ my_flutter_riverpods/lib/myapp/Home.dart
@@ -8,8 +8,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int n = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Riverpod"),
+      ),
+      body:
+      Center(
+        child: Text("$n" , style: const TextStyle(color: Colors.black , fontSize: 24, ),),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.add,color: Colors.white),
+      ),
+    );
   }
 }
